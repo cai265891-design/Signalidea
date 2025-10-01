03be65aa497579631aec5f5b325c0888324b7423
caihongjia <975507571@qq.com>
2025-10-01 20:09:33 +0800

    修复 构建问题

--- apps/nextjs/src/app/[lang]/(dashboard)/dashboard/page.tsx
+++ apps/nextjs/src/app/[lang]/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 import { authOptions, getCurrentUser } from "@saasfly/auth";
 import {
   Table,
